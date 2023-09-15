7c133ede7236100caaa0e83f520bbfa4ef4acd09
Chiken <24449663+KenYasuge@users.noreply.github.com>
2023-09-15 12:41:22 +0800

    Added images for player & computer choice + rearranged some codes

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ const choices = ['rock', 'paper', 'scissors']
 let playerScore = 0;
 let computerScore = 0;
 
+// get score selectors
+const playerScoreText = document.querySelector('.player-score');
+const compScoreText = document.querySelector('.comp-score');
+
+// get player choice image & comp choice image
+const playerImg = document.querySelector('.player-choice');
+const compImg = document.querySelector('.comp-choice');
+
 // Function for computer to get random choice from choices
 function getComputerChoice() {
     // choices.length to get length of array
@@ -39,38 +47,51 @@ function playRound(playerSelection, computerSelection){
         computerScore++
     }
 
+    // show computer selection image
+    if (computerSelection == 'rock'){
+        compImg.src = 'images/rock.png'
+    } else if (computerSelection == 'paper'){
+        compImg.src = 'images/paper.png'
+    } else if (computerSelection == 'scissors'){
+        compImg.src = 'images/scissors.png'
+    }
+
     // show scores on the board
     playerScoreText.textContent = playerScore;
     compScoreText.textContent = computerScore;
 
 }
 
-// get score selectors
-const playerScoreText = document.querySelector('.player-score')
-const compScoreText = document.querySelector('.comp-score')
-
 // on click, player choice = rock
 const rockBtn = document.querySelector('.rock');
 rockBtn.addEventListener('click', function(){
+    // show image of choice
+    playerImg.src = 'images/rock.png';
+
+    // play game
     playRound('rock',getComputerChoice())
-})
+});
 // on click, player choice = paper
 const paperBtn = document.querySelector('.paper');
 paperBtn.addEventListener('click', function(){
+    playerImg.src = 'images/paper.png';
     playRound('paper',getComputerChoice())
-})
+});
 // on click, player choice = scissors
 const scissorsBtn = document.querySelector('.scissors');
 scissorsBtn.addEventListener('click', function(){
+    playerImg.src = 'images/scissors.png';
     playRound('scissors',getComputerChoice())
-})
+});
 
-
-const resetBtn = document.querySelector('.reset')
+// reset button
+const resetBtn = document.querySelector('.reset');
 resetBtn.addEventListener('click', () => {
     // Reset scores for next game
     playerScore = 0;
     computerScore = 0;
     playerScoreText.textContent = playerScore;
     compScoreText.textContent = computerScore;
-})
+    playerImg.src = '';
+    compImg.src = '';
+});
